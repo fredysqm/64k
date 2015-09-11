1fc1d8138ac841b7bb58776b8cc7ebd085595851
Fredy <fredysqm@gmail.com>
2015-09-10 19:40:57 -0500

    agregando compress

--- app/migrations/0001_initial.py
+++ app/migrations/0001_initial.py
@@ -13,16 +13,15 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='opciones',
             fields=[
-                ('clave', models.CharField(max_length=20, primary_key=True, serialize=False)),
+                ('clave', models.CharField(primary_key=True, max_length=20, serialize=False)),
                 ('valor', models.CharField(max_length=20)),
             ],
         ),
         migrations.CreateModel(
             name='slink',
             fields=[
-                ('id', models.AutoField(primary_key=True, auto_created=True, serialize=False, verbose_name='ID')),
-                ('clave', models.CharField(max_length=16, unique=True)),
-                ('url', models.URLField()),
+                ('clave', models.CharField(primary_key=True, max_length=16, serialize=False)),
+                ('url', models.URLField(unique=True)),
                 ('fecha', models.DateTimeField(auto_now=True)),
                 ('clicks', models.PositiveIntegerField(default=0)),
             ],
--- main/settings/_prod.py
+++ main/settings/_prod.py
@@ -10,6 +10,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app',
+    'crispy_forms',
 )
 
 MIDDLEWARE_CLASSES = (
