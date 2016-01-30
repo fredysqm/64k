e1aefef0be4956d5f7c670c10b282cd1cadfcce7
Fredy Quispe <fredysqm@gmail.com>
2016-01-30 09:23:00 -0500

    Issue #1

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,8 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 
 from app.views import *
 
-urlpatterns = patterns('',
+urlpatterns = (
     url(r'^$', slink_crear_view.as_view(), name='slink_crear_url'),
     url(r'^ver/(?P<slug>[-_\w]+)/$', slink_ver_view.as_view(), name='slink_ver_url'),
     url(r'^(?P<slug>[-_\w]+)/$', slink_redirect_view.as_view(), name='slink_redirect_url'),
