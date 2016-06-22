b26565c5506bf1215da895bc5ce9a5a9ace16e50
Fredy Quispe <fredysqm@gmail.com>
2016-06-22 17:02:11 -0500

    previo a cambio de logica

--- api/views.py
+++ api/views.py
@@ -17,6 +17,7 @@ class SlinkAPIView(APIView):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
+
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
