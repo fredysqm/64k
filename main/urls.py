00a02fbe04c78882b1789b78e794e13ef4f5f9cc
Fredy Quispe <fredysqm@gmail.com>
2018-07-02 21:33:02 -0500

    django 2.0.7

--- main/urls.py
+++ main/urls.py
@@ -1,19 +1,18 @@
-from django.conf.urls import include, url
+from django.urls import path, include
 from django.contrib import admin
 from app.views import error404, error500
 from django.conf import settings
 
-
 urlpatterns = [
-    url(r'^adminplus/', include(admin.site.urls)),
-    url(r'^api/', include('api.urls')),
-    url(r'^', include('app.urls')),
+    path( 'adminplus/', admin.site.urls ),
+    path( 'api/', include('api.urls') ),
+    path( '', include('app.urls') ),
 ]
-
 handler404 = error404.as_view()
 handler500 = error500.as_view()
 
 if settings.DEBUG:
+    from django.conf.urls import url
     import debug_toolbar
     urlpatterns = [
         url(r'^__debug__/', include(debug_toolbar.urls)),
